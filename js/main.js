aac10b33f42bb27840be8e69498d514c82cda081
noormohammedk <nur.k.muhammed@gmail.com>
2018-03-30 17:48:41 +0530

    added animation using javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function() {
+  var nav = document.getElementsByTagName('nav')[0];
+  window.addEventListener("scroll", function(){
+    if(window.scrollY > 20) {
+      nav.style.opacity = "0.8";
+    } else {
+      nav.style.opacity = "1";
+    }
+  });
+
+  var reviews = document.getElementsByClassName("review")[0];
+  var input = reviews.getElementsByTagName("input")[0];
+  var articleArr = Array.prototype.slice.call(reviews.getElementsByTagName("article"));
+  input.addEventListener("click", function(){
+    articleArr.forEach(function (elm) {
+      elm.classList.add("clicked");
+    });
+  });
+}());
